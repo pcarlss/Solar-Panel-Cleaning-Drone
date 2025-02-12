4dbe0dffefe8ff4b427f307cddb1dea980967aa5
q1an-w <qianywang25@gmail.com>
2025-02-12 18:09:58 -0500

    old removed

--- Simulations/Rover-Autonomous-Pathing/Sensors/LimitSwitch.py
+++
@@ -1,9 +0,0 @@
-class LimitSwitch:
-    def __init__(self):
-        self.detected_edge = False
-
-    def detect_edge(self, solar_panel_area, rover_position):
-        return self.detected_edge  
-
-    def is_pressed(self):
-        return self.detected_edge
